74123128d16e303487c0eed1a3f40149ebf8b0de
FredYuL <70611716+FredYuL@users.noreply.github.com>
2021-03-07 20:34:37 -0500

    Update Grading_system.py

--- Grading system/Grading_system.py
+++ Grading system/Grading_system.py
@@ -1,4 +1,5 @@
 from compileable import *
+from diff import diff
 
 # Compileable
 compile_bool = compileable_fun('/home/tony/research/test.c')
@@ -7,6 +8,10 @@ if (compile_bool == 0):         # If the input is not compile able, final grade 
   grade_final = 0
   
 # Diff
+
+hwNum = 1  # homework number
+subPath = '/Users/liang/PycharmProjects/pythonProject/research/diffs/'  # this is for testing, need from database
+diff(hwNum, subPath)  # homework number, path of files need diff
 
 # Check memory
 
