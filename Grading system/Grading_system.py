085cb1dc61661f0996b608f90ff686d29b11da0a
Lznasta <70188412+Lznasta@users.noreply.github.com>
2021-03-07 20:22:29 -0500

    3

--- Grading system/Grading_system.py
+++ Grading system/Grading_system.py
@@ -1,6 +1,11 @@
+from compileable import *
 
 # Compileable
+compile_bool = compileable_fun('/home/tony/research/test.c')
 
+if (compile_bool == 0):         # If the input is not compile able, final grade = 0
+  grade_final = 0
+  
 # Diff
 
 # Check memory
