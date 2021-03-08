f998a02245b2cf3ed964f28f25cab04d0109b7d1
Lznasta <70188412+Lznasta@users.noreply.github.com>
2021-03-07 20:16:33 -0500

    3

--- Grading system/compileable.py
+++ Grading system/compileable.py
@@ -1,8 +1,8 @@
-def compileable(inputfilename):
-    import os
-    from os import path
-    from distutils.ccompiler import new_compiler
+import os
+from os import path
+from distutils.ccompiler import new_compiler
 
+def compileable_fun(inputfilename):
     inputfilename = '/home/tony/research/test.c'    # student submission path in database
 
     compiler = new_compiler()
@@ -10,5 +10,7 @@ def compileable(inputfilename):
         compiler.compile([inputfilename])
         compiler.link_executable(['test.o'], 'test')
         print("True1")
+        return 1
     except:
         print("False1")
+        return 0
