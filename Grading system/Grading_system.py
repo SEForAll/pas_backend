29055cf06b006a626f80c28c08e22a2b0574d820
Lznasta <70188412+Lznasta@users.noreply.github.com>
2021-03-22 00:28:34 -0400

    Update Grading_system.py

--- Grading system/Grading_system.py
+++ Grading system/Grading_system.py
@@ -1,17 +1,22 @@
-# input: student file pass
-# output: grade, a string about feedback
+# input: a string which is student file pass
+# output: a tuple that has <grade, a list about feedback(each element is a string)>
+
+import os
+from os import path
+from distutils.ccompiler import new_compiler
 
 def grade_a_file_fun(inputfilepass):
-    from compileable import *
     from diff import diff
-
+    # inputfilepass = '/home/tony/research/test.c'    # student submission path in database
+    
     # Compileable
-    compile_bool = compileable_fun('/home/tony/research/test.c')
-
+    compile_bool = compileable_fun(inputfilepass)
+    
     if (compile_bool == 0):  # If the input is not compile able, final grade = 0
-        print("not compileable")
+        list_final = []
+        list_final.append = 'Your code is not compile able, please check gcc errors.'
         grade_final = 0
-        return grade_final
+        return grade_final, list_final
 
     # Diff
     hwNum = 1  # homework number, need from database
@@ -26,3 +31,18 @@ def grade_a_file_fun(inputfilepass):
     # clean the work space
 
     return grade_final
+
+# Input: student's file pass
+# Output: 0 if not compile able, 1 if compile able.
+def compileable_fun(inputfilepass):
+    # inputfilepass = '/home/tony/research/test.c'    # student submission path in database
+    
+    compiler = new_compiler()
+    try:
+        compiler.compile([inputfilepass])
+        compiler.link_executable(['test.o'], 'test')
+        #print("True1")
+        return 1
+    except:
+        #print("False1")
+        return 0
