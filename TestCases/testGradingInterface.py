059edfd449133e687dcb273976ea6a0f1debb4b6
Jonathan Doorn <jdoorn@purdue.edu>
2021-04-02 10:41:51 -0400

    Added test cases for grading interface

--- TestCases/testGradingInterface.py
+++ TestCases/testGradingInterface.py
@@ -6,7 +6,8 @@ import os
 class TestGradingInterface(unittest.TestCase):
 
     def test_grade_submission(self):
-        graded = interface.grade_submission(os.path.join(os.getcwd(), 'sort.zip'), os.path.join(os.getcwd(), 'Sort2TestCases'))
+        graded = interface.grade_submission(os.path.join(os.getcwd(), 'sort.zip'),
+                                            os.path.join(os.getcwd(), 'Sort2TestCases'))
         self.assertAlmostEqual(graded.get_grade(), 100)
 
 
