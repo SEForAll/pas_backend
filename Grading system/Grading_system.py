66474de2fc382575ba9e297c4585401c454b06b0
Lznasta <70188412+Lznasta@users.noreply.github.com>
2021-04-05 15:59:35 -0400

    Update Grading_system.py

--- Grading system/Grading_system.py
+++ Grading system/Grading_system.py
@@ -3,37 +3,24 @@ from distutils.ccompiler import new_compiler
 import filecmp
 import re
 
-
-# 4. where do you run the program
 # 5. we need to set timeout for any os or make !!!!!!!!!
-# 7. what if there are multiple inputfiles and outputfiles. what if there are no outputfile.
 # pathin and pathout are not being used at all, so we need to look into whether we really need them or not.
 
 # input: a string which is student file pass
 # output: a tuple that has <grade, a list about feedback(each element is a string)>
-def grade_fun(path, pathin, pathout, makefile):
+def grade(path):
     """
     :param path: path to the project. ex) /users/alex/myfiles/project
     :type path: str
-    :param pathin: path to the expected input files. ex) /users/alex/myfiles/project/inputs
-    :type pathin str
-    :param pathout: path to the expected output files. ex) /users/alex/myfiles/project/outputs
-    :type pathout str
-    :param makefile: path to the expected makefile. ex) /users/alex/makefiles/project1
-    :type makefile str
     :return: grade, feedback
     """
+
     list_final = []
     grade_final = 100
 
     debugging = False  # enable debugging print statements
 
-    with open(makefile, 'r') as make:
-        makefiletext = make.read()  # read the makefile from the professor
-
     os.chdir(path)
-    with open('makefile', 'w+') as make:
-        make.write(makefiletext)  # write the makefile from the professor into the hw directory
     os.system('make clean >/dev/null 2>&1')
 
     if debugging:
@@ -41,21 +28,24 @@ def grade_fun(path, pathin, pathout, makefile):
 
     # ---------------------------------
     # check that everything can compile
-    compiler = new_compiler()
+    # compiler = new_compiler()
 
     if len(os.listdir(path)) == 0:
         list_final.append('no files submitted')
         return 0, list_final
 
+    compiler = new_compiler()
+
     for filename in os.listdir(path):  # for all files in the directory
         if filename.endswith(".c"):  # that end with .c
             try:
-                compiler.compile([filename])  # check to see that it compiles
+                compiler.compile([filename])  # check to see that it compiled
                 list_final.append(f'{filename} compiled correctly!')
 
             except:
                 list_final.append(f'{filename} did not compile correctly...')
                 return 0, list_final
+
         else:
             continue
 
@@ -66,33 +56,36 @@ def grade_fun(path, pathin, pathout, makefile):
 
     numberoftestcases = 0
 
-    with open("makefile", 'r') as f:  # open the makefile
+    with open("Makefile", 'r') as f:  # open the Makefile
         text = f.read()
 
         # -----------------------
         # get the name of the executable
 
-        ex = re.compile(r'-o (\w+)')  # use regex to get the name of the executable made from the makefile
+        ex = re.compile(r'-o (\w+)')  # use regex to get the name of the executable made from the Makefile
         match = ex.search(text)
         if match is not None:
             executable = match.group(1)  # set the name of the executable as long as it is able to find it
         else:
-            list_final.append('error when executing makefile... contact your professor about this issue (name of executable can not be found)')
+            list_final.append(
+                'error when executing Makefile... contact your professor about this issue (name of executable can not be found)')
             return 0, list_final
 
         # ------------------------
         # get the number of test cases to run
 
-        num = re.compile(r'testcase(\d+):')
+        num = re.compile(r'test(\d+):')
         match = num.findall(text)
         if len(match) != 0:
             numberoftestcases = int(match[-1])
         else:
-            list_final.append('error when executing makefile... contact your professor about this issue (number of test cases could not be found)')
+            list_final.append(
+                'error when executing Makefile... contact your professor about this issue (number of test cases could not be found)')
             return 0, list_final
 
         if numberoftestcases == 0:
-            list_final.append('error when executing makefile... contact your professor about this issue (number of test cases is not correct)')
+            list_final.append(
+                'error when executing Makefile... contact your professor about this issue (number of test cases is not correct)')
             return 0, list_final
 
         # ------------------------
@@ -111,18 +104,19 @@ def grade_fun(path, pathin, pathout, makefile):
     os.chdir(path)
     with open('empty.txt', 'w+') as f:
         f.write('')
-   
+
     with open('grade.txt', 'w+') as f:
-       f.write('')
+        f.write('')
 
     for i in range(1, numberoftestcases + 1):
-        #print(f'i is {i}')
+        # print(f'i is {i}')
         os.system('make clean >/dev/null 2>&1')
         os.system('make >/dev/null 2>&1')
-        os.system(f'make testcase{i} >/dev/null 2>&1')  # PROF MUST SEND THE OUTPUT OF THE DIFF COMMAND TO grade.txt !!!!!!
-                                        # i can't route the output of the diff command, only the output of the testcase command
-                                        # this must be done by the professor
-                                        # ex) diff output1.txt expected1.txt > grade.txt
+        os.system(
+            f'make testcase{i} >/dev/null 2>&1')  # PROF MUST SEND THE OUTPUT OF THE DIFF COMMAND TO grade.txt !!!!!!
+        # i can't route the output of the diff command, only the output of the testcase command
+        # this must be done by the professor
+        # ex) diff output1.txt expected1.txt > grade.txt
 
         comp = filecmp.cmp('grade.txt', 'empty.txt', shallow=False)
         if comp is True:
@@ -131,7 +125,7 @@ def grade_fun(path, pathin, pathout, makefile):
         else:
             list_final.append("Test case " + str(i) + " is wrong...")
 
-    grade_final -= 100/numberoftestcases * (numberoftestcases - passed)
+    grade_final -= 100 / numberoftestcases * (numberoftestcases - passed)
     list_final.append(f'{passed}/{numberoftestcases} test cases passed!')
 
     if debugging:
@@ -141,15 +135,15 @@ def grade_fun(path, pathin, pathout, makefile):
     # Check memory
 
     bytesLeaked, blocksLeaked = memcheck(path, valgrindstatements)
-
+    # print(bytesLeaked)
     if bytesLeaked < 0:
-        list_final.append('error when executing makefile... contact your professor about this issue')
+        list_final.append('error when executing Makefile... contact your professor about this issue')
         return 0, list_final
     # else:
     #     list_final.append('makefile executed correctly!')
 
     if bytesLeaked > 0:
-        list_final.append(f'{bytesLeaked} byte(s) of memory leak was present in the program')
+        list_final.append(f'{bytesLeaked} byte(s) of memory leak present in the program')
         grade_final -= bytesLeaked
     if bytesLeaked == 0:
         list_final.append('No memory leak!')
@@ -204,6 +198,6 @@ def memcheck(makefile_dir, valgrindstatements):
             bytesInUse += int(match.group(1))  # put regex groups from the match into variables and cast to integers
             blocks += int(match.group(2))
 
-    os.remove(tempfile)  # remove the files we created as they only pertain to this function
+        os.remove(tempfile)  # remove the files we created as they only pertain to this function
 
     return bytesInUse, blocks
