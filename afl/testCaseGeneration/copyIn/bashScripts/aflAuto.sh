609fba1ad96b3ef8dff985b9d995c9485c9865b9
Jack <myers395@purdue.edu>
2021-06-10 18:36:23 -0400

    Continued work on genTestCases.py

--- afl/testCaseGeneration/copyIn/bashScripts/aflAuto.sh
+++ afl/testCaseGeneration/copyIn/bashScripts/aflAuto.sh
@@ -7,10 +7,12 @@ if [ $# -ne 4 ]; then
 	echo "Variable Mismatch"
 	exit 1
 fi
-touch $3"/log.txt"
+cd $3
+rm -r *
 cd $1
-rm -r CMakeCache.txt CMakeFiles Makefile cmake_install.cmake binary >> $3"/log.txt" 
-cmake CMakeLists.txt >> $3"/log.txt"
+cmake CMakeLists.txt > $3"/log.txt"
 make >> $3"/log.txt"
+rm -r CMakeCache.txt CMakeFiles cmake_install.cmake CMakeLists.txt Makefile
 export AFL_NO_UI=1
 afl-fuzz -i $2 -o $3 -V $4 -- $1"/binary" >> $3"/log.txt"
+rm $1"/binary"
--- afl/testCaseGeneration/genTestCases.py
+++ afl/testCaseGeneration/genTestCases.py
@@ -6,23 +6,46 @@ import tarfile
 from io import BytesIO
 
 def run(codeDir, inputDir, outputDir, time):
+    populateFiles(codeDir, inputDir)
     client = docker.from_env()
-    #container = client.containers.run("aflplusplus/aflandcmake", "bash", detach=True)
-    container = client.containers.run("aflplusplus/aflandcmake", "ls /mnt" , volumes={os.getcwd() + "/copyIn": {'bind':'/mnt', 'mode': 'rw'}}, detach=True, tty=True, name="workingContainer")
+    container = client.containers.run("aflplusplus/aflandcmake", "/mnt/bashScripts/aflAuto.sh /mnt/code /mnt/inputs /mnt/output 10" , volumes={os.getcwd() + "/copyIn": {'bind':'/mnt', 'mode': 'rw'}}, detach=True, tty=True, name="workingContainer")
     container.start()
     print(container.name)
     #container.exec_run
     container.stop()
-    print(container.attach(stdout=True))
     container.remove()
 
     print("Script Complete")
 
-def populateFiles():
+def populateFiles(codeDir, inputDir):
+    genCMake(os.getcwd() + "/copyIn/code")
     return
 
-def copyToRoot(path):
-    return
+def genCMake(dir_path):
+    #@author: leila_i3viefa
+
+    #dir_path = os.path.dirname(os.path.realpath('C:/afl/code SAMPLE')) #creates variable of the folder to search
+    mainfile = open(dir_path + "/CMakeLists.txt",'w') #creates text file
+
+    #list of most of the content for the text file since only the last line of the file changes
+    lines = ['cmake_minimum_required(VERSION 3.16)\n', '\n', 'project(binary)\n', '\n', 'set(AFL_FUZZER_DIR "" CACHE STRING "AFL fuzzer binary directory")\n', 'if(AFL_FUZZER_DIR)\n', '    set(CMAKE_C_COMPILER "${AFL_FUZZER_DIR}/afl-clang-lto")\n', 'else()\n', '    set(CMAKE_C_COMPILER afl-clang-lto)\n', 'endif()\n', '\n']
+
+    for element in lines:
+        #writes the lines list to the text file
+        mainfile.write(element)
+
+    lineLast = "add_executable(${PROJECT_NAME}" #first part of the last line of text file
+
+    for root,dirs,files in os.walk(dir_path): 
+        for file in files:
+            if file.endswith('.c'):
+                #singles out the files with .c ending
+                lineLast = lineLast + " " + str(file) #adds the file name to the string of the last line
+
+    lineLast = lineLast + ")"
+
+    mainfile.write(lineLast) #adds the last line to the text file
+    mainfile.close() #closes the text file
 
 if __name__ == "__main__":
-    run("~/temp/push/test/code", "~/temp/push/test/inputs", "~/temp/push/test/outputs", 10)
+    run("~/git/pas_backend/afl/testCaseGeneration/test/code", "~/git/pas_backend/afl/testCaseGeneration/test/inputs", "~/git/pas_backend/afl/testCaseGeneration/test/outputs", 10)
