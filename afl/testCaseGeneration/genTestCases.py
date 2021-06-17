f9bed273ee79574bddcc8da5fbe800a2e5f20c30
Yu Liang <liang289@scholar-fe05.rcac.purdue.edu>
2021-06-17 18:14:09 -0400

    copydelete

--- afl/testCaseGeneration/genTestCases.py
+++ afl/testCaseGeneration/genTestCases.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
+import shutil
 from shutil import copyfile
-import docker
+#import docker
 import os
 import subprocess
 import tarfile
@@ -7,19 +9,55 @@ from io import BytesIO
 
 def run(codeDir, inputDir, outputDir, time):
     populateFiles(codeDir, inputDir)
-    client = docker.from_env()
-    container = client.containers.run("aflplusplus/aflandcmake", "/mnt/bashScripts/aflAuto.sh /mnt/code /mnt/inputs /mnt/output 10" , volumes={os.getcwd() + "/copyIn": {'bind':'/mnt', 'mode': 'rw'}}, detach=True, tty=True, name="workingContainer")
-    container.start()
-    print(container.name)
+    #client = docker.from_env()
+    #container = client.containers.run("aflplusplus/aflandcmake", "/mnt/bashScripts/aflAuto.sh /mnt/code /mnt/inputs /mnt/output 10" , volumes={os.getcwd() + "/copyIn": {'bind':'/mnt', 'mode': 'rw'}}, detach=True, tty=True, name="workingContainer")
+    #container.start()
+    #print(container.name)
     #container.exec_run
-    container.stop()
-    container.remove()
+    #container.stop()
+    deletefiles(os.getcwd() + "/copyIn/code/", os.getcwd() + "/copyIn/inputs/", outputDir)
+    #container.remove()
 
     print("Script Complete")
 
 def populateFiles(codeDir, inputDir):
-    genCMake(os.getcwd() + "/copyIn/code")
+    copyfiles(codeDir, inputDir, os.getcwd() + "/copyIn/code/", os.getcwd() + "/copyIn/inputs/")
+    genCMake(os.getcwd() + "/copyIn/code/")
     return
+
+
+def copyfiles(codeDir, inputDir, codeToDir, inputToDir):
+    # name of all the files in directories
+    listcode = os.listdir(codeDir) 
+    listinputs = os.listdir(inputDir)
+    
+    # copy files to directories
+    for code in listcode:
+        go = shutil.copy(codeDir + code, codeToDir) 
+    for inputs in listinputs:
+        go2 = shutil.copy(inputDir + inputs, inputToDir)
+
+    # creating output directory(if needed)
+    # os.mkdir(os.path.join(os.getcwd() + "/test/","outputs"))
+
+def deletefiles(codeToDir, inputToDir, outputDir):
+    # name of all the files in directories
+    listcodeTo = os.listdir(codeToDir)
+    listinputsTo = os.listdir(inputToDir)
+    listoutputs = os.listdir(outputDir)
+
+    # delete all the files in directories
+    for i in listcodeTo:
+        os.remove(codeToDir + i)
+
+    for j in listinputsTo:
+        os.remove(inputToDir + j)
+
+    for k in listoutputs:
+        os.remove(outputDir + k)
+    
+    # delete output directory(if needed)
+    # os.rmdir(os.getcwd() + "/test/outputs/")
 
 def genCMake(dir_path):
     #@author: leila_i3viefa
