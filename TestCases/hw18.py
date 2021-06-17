69a66cb86a44160153226dce3216d89f52f060a3
Yu-Nie <mrcrystalnie@gmail.com>
2021-06-17 09:31:24 -0400

    update

--- TestCases/hw18.py
+++ TestCases/hw18.py
@@ -14,6 +14,23 @@ class TestGradingInterface(unittest.TestCase):
         print(graded.get_error_list())
         print(graded.get_grade())
 
+    def test_grade_submission1(self):
+        graded = interface.grade_submission(os.path.join(path, '2020homeworks/grade_testing/hw17-21/hw18/hw18_compile_error.zip'),
+                                            os.path.join(path, '2020homeworks/HW18Huffman2'))
+        print(graded.get_error_list())
+        print(graded.get_grade())
+        
+    def test_grade_submission2(self):
+        graded = interface.grade_submission(os.path.join(path, '2020homeworks/grade_testing/hw17-21/hw18/hw18_memory_leak.zip'),
+                                            os.path.join(path, '2020homeworks/HW18Huffman2'))
+        print(graded.get_error_list())
+        print(graded.get_grade())
+    
+    def test_grade_submission3(self):
+        graded = interface.grade_submission(os.path.join(path, '2020homeworks/grade_testing/hw17-21/hw18/hw18_wrong_output.zip'),
+                                            os.path.join(path, '2020homeworks/HW18Huffman2'))
+        print(graded.get_error_list())
+        print(graded.get_grade())
 
 if __name__ == '__main__':
     unittest.main() # run the unit test again ...
