fcce26d43d9e2b1d3ad07ab9614d3b3533a266b0
Yinhan Chen <chen2855@purdue.edu>
2021-06-17 07:51:11 -0400

    delete ourputs/

---
+++ TestCases/2020homeworks/grade_testing/hw9-16/hw10/hw10_simple_wrong_code/main.c
@@ -0,0 +1,19 @@
+// ***
+// *** DO NOT modify this file
+// ***
+#include <stdlib.h> 
+#include <string.h> 
+#include <stdio.h>
+#include "hw10.h"
+
+int main(int argc, char * * argv)
+{
+  if (argc != 3)
+    {
+      fprintf(stderr, "need two numbers\n");
+      return EXIT_FAILURE;
+    }
+  printf("gg\n");
+  return EXIT_SUCCESS;
+}
+
