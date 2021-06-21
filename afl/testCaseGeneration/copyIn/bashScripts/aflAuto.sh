a92f7325dc2b9c54cd53c4bf92b50554f8713112
Jack <myers395@purdue.edu>
2021-06-21 00:38:38 -0400

    added permission change to aflauto.sh

--- afl/testCaseGeneration/copyIn/bashScripts/aflAuto.sh
+++ afl/testCaseGeneration/copyIn/bashScripts/aflAuto.sh
@@ -13,6 +13,8 @@ cd $1
 cmake CMakeLists.txt &> $3"/log.txt"
 make &>> $3"/log.txt"
 rm -r CMakeCache.txt CMakeFiles cmake_install.cmake CMakeLists.txt Makefile
-export AFL_NO_UI=1
-afl-fuzz -i $2 -o $3 -V $4 -- $1"/binary" >> $3"/log.txt"
-rm $1"/binary"
+#export AFL_NO_UI=1
+timeout $4 afl-fuzz -i $2 -o $3 -- $1"/binary" >> $3"/log.txt"
+printf "Fuzz Complete\n" > $3"/log.txt"
+chmod -R 777 $3"/default"
+printf "Bash Script Complete" >> $3"/log.txt"
